8aaa560beded3fb339c69d711d9003f547ddf2c5
Ovanes <o-zakharyan@mail.ru>
2022-05-03 23:56:52 +0300

    add scrollView and tap gesture recognizer

--- SecondsConvert/ConvertViewController.swift
+++ SecondsConvert/ConvertViewController.swift
@@ -14,6 +14,7 @@ class ConvertViewController: UIViewController {
 
     @IBOutlet weak var settingControl: UISegmentedControl!
     @IBOutlet weak var secondsTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
    
    
@@ -99,9 +100,77 @@ class ConvertViewController: UIViewController {
         secondsTextField.clearButtonMode = .whileEditing    // кнопка очистить (крестик справа)
         secondsTextField.placeholder = "Введите кол-во секунд" // надпись (подсказка)
         
+        // Жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        // Присваиваем его UIScrollVIew
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        
     }
     
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+    }
+
+
+    @IBAction func viewTapped(_ sender: UITapGestureRecognizer) {
+        //view.resignFirstResponder()
+        scrollView.endEditing(true)
+    }
+    
+
+    
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
